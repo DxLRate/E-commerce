5084583be8acc3ee441856e9e966ec30fb4969c1
Sourabh Prasad <saurabh3052000@gmail.com>
2022-08-19 12:10:20 +0530

    frontend completed

---
+++ src/data.jsx
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/zhPqcNb/diwali-removebg-preview.png",
+      title: "Diwali Festive Sale",
+      desc: "This Festive Season get a chance to upgrade with great discounts",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/s1kgbds/spring.png",
+      title: "Spring Collection",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/DR4NkMB/Loungewear.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://canary.contestimg.wish.com/api/webimage/5bbfe985c2cc753340cb71bc-large.jpg?cache_buster=c4a98949bbd901a933e793966f969bbe",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://johnlewis.scene7.com/is/image/johnlewis/the-loungewear-guide",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://cdn.hiconsumption.com/wp-content/uploads/2021/01/Best-Lightweight-Jackets-For-Men-0-Hero.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://i.ibb.co/xCmZjYK/etcjpg-removebg-preview.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://i.ibb.co/XDjpNFm/fancy-ladies-footwear-500x500-removebg-preview.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
